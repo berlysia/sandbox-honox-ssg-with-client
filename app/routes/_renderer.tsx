bb33adc5e4e1ca900eaa47ed748860e2615d3db2
berlysia <berlysia@gmail.com>
2024-02-16 02:58:11 +0900

    update

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,6 +1,14 @@
 import { Style } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
 import { Script } from 'honox/server'
+import { Manifest } from "vite";
+
+const manifestFile = Object.values(
+  import.meta.glob<{ default: Manifest }>("/.hono/.vite/manifest.json", {
+    eager: true,
+  }),
+)[0];
+const manifest = manifestFile.default;
 
 export default jsxRenderer(({ children, title }) => {
   return (
@@ -9,7 +17,7 @@ export default jsxRenderer(({ children, title }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        <Script src="/app/client.ts" />
+        <Script src="/app/client.ts" manifest={manifest}/>
         <Style />
       </head>
       <body>{children}</body>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
-import pages from '@hono/vite-cloudflare-pages'
-import honox from 'honox/vite'
-import client from 'honox/vite/client'
-import { defineConfig } from 'vite'
+import ssg from "@hono/vite-ssg";
+import honox from "honox/vite";
+import client from "honox/vite/client";
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+
+const entry = "app/server.ts";
 
 export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
+  if (mode === "client") {
+    // Client build
     return {
-      plugins: [client()]
-    }
-  } else {
-    return {
-      plugins: [honox(), pages()]
-    }
+      plugins: [client()],
+      build: {
+        outDir: ".hono",
+        emptyOutDir: true,
+      },
+    };
   }
-})
+
+  {
+    // SSG build
+    return {
+      plugins: [
+        honox(),
+        devServer({ entry }),
+        ssg({ entry }),
+        {
+          name: "emptyOutDir",
+          config() {
+            return { build: { emptyOutDir: false } };
+          },
+        },
+      ],
+    };
+  }
+});
